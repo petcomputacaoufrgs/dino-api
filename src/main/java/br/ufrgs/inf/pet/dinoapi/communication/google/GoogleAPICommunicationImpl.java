f4d004e689309a66a2ca4a5293f0105c206c89b3
Joao Pedro SIlveira <jpedross1999@gmail.com>
2020-01-31 17:46:24 -0300

    Fix(ClientSecrets): AJuste na forma de leitura

--- src/main/java/br/ufrgs/inf/pet/dinoapi/communication/google/GoogleAPICommunicationImpl.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/communication/google/GoogleAPICommunicationImpl.java
@@ -9,6 +9,7 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -28,18 +29,21 @@ public class GoogleAPICommunicationImpl implements GoogleAPICommunication {
 
             scopes.add(GoogleScopes.CALENDAR.getScope());
 
-            return new GoogleAuthorizationCodeTokenRequest(
-                    new NetHttpTransport(),
-                    JacksonFactory.getDefaultInstance(),
-                    "https://oauth2.googleapis.com/token",
-                    clientSecrets.getDetails().getClientId(),
-                    clientSecrets.getDetails().getClientSecret(),
-                    token,
-                    REDIRECT_URI)   // Specify the same redirect URI that you use with your web
-                                    // app. If you don't have a web version of your app, you can
-                                    // specify an empty string.
-                    .setScopes(scopes)
-                    .execute();
+            GoogleTokenResponse tokenResponse =
+                    new GoogleAuthorizationCodeTokenRequest(
+                            new NetHttpTransport(),
+                            JacksonFactory.getDefaultInstance(),
+                            "https://oauth2.googleapis.com/token",
+                            clientSecrets.getDetails().getClientId(),
+                            clientSecrets.getDetails().getClientSecret(),
+                            token,
+                            REDIRECT_URI)   // Specify the same redirect URI that you use with your web
+                                            // app. If you don't have a web version of your app, you can
+                                            // specify an empty string.
+                            .setScopes(scopes)
+                            .execute();
+
+            return tokenResponse;
         } catch (IOException ex) {
             //TO-DO Tratar erro
         }
@@ -53,38 +57,30 @@ public class GoogleAPICommunicationImpl implements GoogleAPICommunication {
 
         scopes.add(GoogleScopes.CALENDAR.getScope());
 
-        GoogleClientSecrets secrets = getClientSecrets();
+        GoogleTokenResponse tokenResponse = null;
+        try {
+            GoogleClientSecrets secrets = getClientSecrets();
 
-        if (secrets != null) {
-            GoogleTokenResponse tokenResponse = null;
-            try {
-                tokenResponse = new GoogleRefreshTokenRequest(
-                        new NetHttpTransport(),
-                        JacksonFactory.getDefaultInstance(),
-                        refreshToken,
-                        secrets.getDetails().getClientId(),
-                        secrets.getDetails().getClientSecret())
-                        .setScopes(scopes)
-                        .setGrantType("refresh_token")
-                        .execute();
+            tokenResponse = new GoogleRefreshTokenRequest(
+                    new NetHttpTransport(),
+                    JacksonFactory.getDefaultInstance(),
+                    refreshToken,
+                    secrets.getDetails().getClientId(),
+                    secrets.getDetails().getClientSecret())
+                    .setScopes(scopes)
+                    .setGrantType("refresh_token")
+                    .execute();
 
-                return tokenResponse;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            return tokenResponse;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return null;
     }
 
-    private GoogleClientSecrets getClientSecrets() {
-        GoogleClientSecrets secrets = new GoogleClientSecrets();
-        secrets.getDetails().setAuthUri("https://accounts.google.com/o/oauth2/auth");
-        secrets.getDetails().setClientId("467762039422-lgajbj314vd4ehfq51n8h7abear3pcjk.apps.googleusercontent.com");
-        secrets.getDetails().setClientSecret("W8RsPL9SDhIqJKwsTIUxMx5K");
-        secrets.getDetails().setTokenUri("https://oauth2.googleapis.com/token");
-
-        return secrets;
+    private GoogleClientSecrets getClientSecrets() throws IOException {
+        return GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), new StringReader("{\"web\":{\"client_id\":\"467762039422-lgajbj314vd4ehfq51n8h7abear3pcjk.apps.googleusercontent.com\",\"project_id\":\"dinoapp-264514\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_secret\":\"W8RsPL9SDhIqJKwsTIUxMx5K\"}}"));
     }
 
 }
