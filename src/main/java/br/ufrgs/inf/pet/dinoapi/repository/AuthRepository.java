5490bf7bcfc2e0bee65828687d63abbff3ad4058
Joao Pedro Silveira <jpedross1999@gmail.com>
2020-07-08 13:22:51 -0300

    Refactor(Query): Adicionar Param para parametros de queries ao inves da posição

--- src/main/java/br/ufrgs/inf/pet/dinoapi/repository/AuthRepository.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/repository/AuthRepository.java
@@ -3,6 +3,7 @@ package br.ufrgs.inf.pet.dinoapi.repository;
 import br.ufrgs.inf.pet.dinoapi.entity.Auth;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,6 +15,6 @@ import java.util.Optional;
  */
 @Repository
 public interface AuthRepository extends CrudRepository<Auth, Long> {
-    @Query("SELECT a FROM Auth a WHERE a.accessToken LIKE ?1")
-    Optional<Auth> findByAccessToken(String accessToken);
+    @Query("SELECT a FROM Auth a WHERE a.accessToken LIKE :accessToken")
+    Optional<Auth> findByAccessToken(@Param("accessToken") String accessToken);
 }
