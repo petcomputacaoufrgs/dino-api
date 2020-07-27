6cd81b43d75c4e1f6d0afdfc0a536c30877e0bf6
Mayra <mayra.cademartori@gmail.com>
2020-07-27 20:53:21 -0300

    Feat(Delete and Response Models)

---
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/model/contacts/ContactResponseModel.java
@@ -0,0 +1,42 @@
+package br.ufrgs.inf.pet.dinoapi.model.contacts;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactResponseModel {
+
+    Long version;
+
+    List<ContactModel> responseModels;
+
+    public ContactResponseModel() {
+        this.setResponseModels(new ArrayList<>());
+    }
+
+    public ContactResponseModel(Long version, ContactModel model) {
+        List<ContactModel> models = new ArrayList<>();
+        models.add(model);
+        this.setVersion(version);
+        this.setResponseModels(models);
+    }
+
+    public ContactResponseModel(Long version, List<ContactModel> models) {
+        this.setVersion(version);
+        this.setResponseModels(models);
+    }
+
+        public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public List<ContactModel> getResponseModels() {
+        return responseModels;
+    }
+
+    public void setResponseModels(List<ContactModel> responseModels) {
+        this.responseModels = responseModels;
+    }
+}
