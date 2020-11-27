648b758740bc7809da87d70e54251d46d9da136d
Joao Pedro <jpedross1999@gmail.com>
2020-11-27 20:19:11 -0300

    Feat(Controller): Adicionar rota

--- src/main/java/br/ufrgs/inf/pet/dinoapi/controller/synchronizable/SynchronizableController.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/controller/synchronizable/SynchronizableController.java
@@ -4,6 +4,7 @@ import br.ufrgs.inf.pet.dinoapi.entity.synchronizable.SynchronizableEntity;
 import br.ufrgs.inf.pet.dinoapi.model.synchronizable.SynchronizableModel;
 import br.ufrgs.inf.pet.dinoapi.service.synchronizable.SynchronizableService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
 
 public abstract class SynchronizableController<T extends SynchronizableEntity> {
     protected final SynchronizableService<T> service;
@@ -12,6 +13,7 @@ public abstract class SynchronizableController<T extends SynchronizableEntity> {
         this.service = service;
     }
 
+    @PutMapping("sync/")
     public ResponseEntity<?> updateOrder(SynchronizableModel<T> model) {
         return this.service.synchronize(model);
     }
