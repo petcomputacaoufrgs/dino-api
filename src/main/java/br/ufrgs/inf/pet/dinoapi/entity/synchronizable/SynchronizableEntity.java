793cefb663e1f67144399792f62b57ae9770367a
Joao Pedro <jpedross1999@gmail.com>
2020-11-27 20:26:23 -0300

    Feat(Entity): Adicionar atributos de persistencia

--- src/main/java/br/ufrgs/inf/pet/dinoapi/entity/synchronizable/SynchronizableEntity.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/entity/synchronizable/SynchronizableEntity.java
@@ -2,17 +2,35 @@ package br.ufrgs.inf.pet.dinoapi.entity.synchronizable;
 
 import br.ufrgs.inf.pet.dinoapi.model.synchronizable.SynchronizableModel;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
+import static javax.persistence.GenerationType.AUTO;
 
+@Entity
 public abstract class SynchronizableEntity<T extends SynchronizableEntity> {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    @Column(name = "id", nullable = false)
     protected Long id;
 
+    @Column(name = "last_update", nullable = false)
     protected Date lastUpdate;
 
-    public abstract Date getLastUpdate();
+    public SynchronizableEntity() {
+        this.lastUpdate = new Date();
+    }
 
-    public abstract Long getId();
+    public Date getLastUpdate() {
+        return this.lastUpdate;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
 
     public boolean isMoreUpdated(SynchronizableModel<T> model) {
         final Date thisLastUpdate = this.getLastUpdate();
