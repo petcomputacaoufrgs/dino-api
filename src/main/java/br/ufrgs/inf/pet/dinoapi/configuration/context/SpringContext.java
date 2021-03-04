193e6edf46d79b7571084e16070cf82d3683c241
João Pedro <jpedross1999@gmail.com>
2021-03-04 12:13:02 -0300

    Feat(MailSender): Configuration

--- src/main/java/br/ufrgs/inf/pet/dinoapi/configuration/context/SpringContext.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/configuration/context/SpringContext.java
@@ -10,13 +10,7 @@ public class SpringContext implements ApplicationContextAware {
 
     private static ApplicationContext context;
 
-    /**
-     * Returns the Spring managed bean instance of the given class type if it exists.
-     * Returns null otherwise.
-     * @param beanClass
-     * @return
-     */
-    public static <T extends Object> T getBean(Class<T> beanClass) {
+    public static <T> T getBean(Class<T> beanClass) {
         return context.getBean(beanClass);
     }
 
--- src/main/java/br/ufrgs/inf/pet/dinoapi/controller/test_connection/TestConnectionControllerImpl.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/controller/test_connection/TestConnectionControllerImpl.java
@@ -3,18 +3,30 @@ package br.ufrgs.inf.pet.dinoapi.controller.test_connection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestConnectionControllerImpl implements TestConnectionController {
 
+    private JavaMailSender javaMailSender;
+
     @Autowired
-    public TestConnectionControllerImpl() { }
+    public TestConnectionControllerImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
     @Override
     @GetMapping("public/test_connection/")
     public ResponseEntity<Void> get() {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("jpedross1999@gmail.com");
+        msg.setSubject("Tudo bem?");
+        msg.setText("Olá, tudo bem?");
+
+        javaMailSender.send(msg);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
