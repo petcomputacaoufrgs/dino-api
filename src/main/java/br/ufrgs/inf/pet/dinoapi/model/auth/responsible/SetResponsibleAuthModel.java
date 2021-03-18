c1b08e6f16a6fe7f6f41940e727bc28ecfd30d3e
João Pedro <jpedross1999@gmail.com>
2021-03-18 18:55:39 -0300

    Feat(Password request): Request password on app start and on password changed

--- src/main/java/br/ufrgs/inf/pet/dinoapi/model/auth/responsible/SetResponsibleAuthModel.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/model/auth/responsible/SetResponsibleAuthModel.java
@@ -2,11 +2,11 @@ package br.ufrgs.inf.pet.dinoapi.model.auth.responsible;
 
 import br.ufrgs.inf.pet.dinoapi.constants.UserSettingsConstants;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class SetResponsibleAuthModel {
-    @NotNull(message = UserSettingsConstants.KEY_NULL_MESSAGE)
+    @NotBlank(message = UserSettingsConstants.KEY_NULL_MESSAGE)
     @Size(min = UserSettingsConstants.KEY_MIN, max = UserSettingsConstants.KEY_MAX, message = UserSettingsConstants.KEY_SIZE_MESSAGE)
     private String key;
 
--- src/main/java/br/ufrgs/inf/pet/dinoapi/model/auth/responsible/VerifyResponsibleRecoverCodeModel.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/model/auth/responsible/VerifyResponsibleRecoverCodeModel.java
@@ -2,10 +2,11 @@ package br.ufrgs.inf.pet.dinoapi.model.auth.responsible;
 
 import br.ufrgs.inf.pet.dinoapi.constants.RecoverPasswordConstants;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public class VerifyResponsibleRecoverCodeModel {
-    @NotNull(message = RecoverPasswordConstants.NULL_CODE_MESSAGE)
+    @NotBlank(message = RecoverPasswordConstants.NULL_CODE_MESSAGE)
     private String code;
 
     public String getCode() {
